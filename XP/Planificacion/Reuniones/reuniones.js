80d0aeb80a4f063c1e318b31d82989f4a7d728df
Angel Anguaya <angel.anguaya02@epn.edu.ec>
2025-12-30 23:23:52 -0500

    Artefacto reuniones completa

---
+++ XP/Planificacion/Reuniones/reuniones.js
@@ -0,0 +1,24 @@
+// Datos de reuniones
+const reunionesData = [
+  {
+    id: 1,
+    titulo: 'Repartición de roles e inicio de la planificación',
+    fecha: '2025-12-29',
+    objetivo: 'Repartir roles y artefactos',
+    queSeHizo: 'Se repartió los roles, luego se hizo grupos para asignarnos los distintos artefactos de la planificación, diseño y desarrollo.'
+  },
+  {
+    id: 2,
+    titulo: 'Reunión para hacer los artefactos',
+    fecha: '2025-12-30',
+    objetivo: 'Realizar los artefactos de la fase de planificación correspondiente al plan de entrega, rotaciones y reuniones.',
+    queSeHizo: 'Se realizó tres releases en la parte de plan de entrega y se añadió las historias de usuario, luego en las rotaciones se asignó los desarrolladores y testers que van a rotar y en cuanto tiempo se va a rotar, y se hizo el artefacto de las reuniones.'
+  },
+  {
+    id: 3,
+    titulo: 'Revisión de planificación',
+    fecha: '2026-01-01',
+    objetivo: 'Revisar los artefactos correspondientes a la fase de planificación',
+    queSeHizo: '.'
+  }
+];
